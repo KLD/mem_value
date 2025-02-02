bc25e847a9d06224aecfd9b77bbfa877a0d77724
KLD <kld0093@hotmail.com>
2025-02-02 12:37:14 +0300

    Remove required in NMemObject

--- lib/src/types/n_mem_object.dart
+++ lib/src/types/n_mem_object.dart
@@ -3,7 +3,7 @@ import '../core/mem_value.dart';
 
 /// Uses [MemSerializable] to store and retrieve a custom object.
 class NMemObject<T extends MemSerializable> extends MemValue<T?> {
-  NMemObject(super.tag, {required super.initValue, super.persist});
+  NMemObject(super.tag, {super.initValue, super.persist});
 
   @override
   T parse(String value) => this.value!.parse(value);
